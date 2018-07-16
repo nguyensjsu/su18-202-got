2047e192488aa0f5a9009c98e4072421d749a14d
yuxu <yzx105psu@gmail.com>
2018-07-16 00:04:31 -0700

    add Store REST API v1

---
+++ express_server/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  GOOGLE_PLACE_API: "AIzaSyBeNuRd1ypxU34H6QCt9VxJ7f0b1msJjKE"
+};
--- express_server/index.js
+++ express_server/index.js
@@ -1,8 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const {GOOGLE_PLACE_API} = require("./config/keys");
+const axios = require("axios");
+const rootAPI = "https://maps.googleapis.com/maps/api/place/textsearch/json?";
 
-app.get('/', (req, res) =>{
-  res.send('Hello world!');
+app.get("/store", async (req, res) => {
+  const { data } = await axios.get(rootAPI, {
+    params: {
+      query: req.query.query,
+      key: GOOGLE_PLACE_API
+    }
+  });
+  res.send(data);
 });
 
 app.listen(5000);
