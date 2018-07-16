f5fd5666554b2b89d8787d6eb46d0989035aa7a3
manojknit <erpatel@gmail.com>
2018-07-15 22:47:50 -0700

    initial commit for POC

---
+++ springboot-mongodb-got-api/src/main/java/com/got/springbootmongodbgot/SpringbootMongodbGotApplication.java
@@ -0,0 +1,12 @@
+package com.got.springbootmongodbgot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbGotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMongodbGotApplication.class, args);
+    }
+}
