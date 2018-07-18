8f4f800ef9a451b2fd61d3fa993a01486a886068
yuxu <yzx105psu@gmail.com>
2018-07-17 19:53:32 -0700

    finished sso heroku deployment

---
+++ express_server/sso_server/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  mongoDBURI: process.env.MONGODB_URI,
+  cookieKey: process.env.COOKIE_KEY
+}
