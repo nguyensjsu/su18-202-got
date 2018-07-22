ea47668d57c9e85c63549b55108c3bc20e2d5b38
renato <my@email.com>
2018-07-21 23:13:21 -0700

    adding db.php file that handles the database connection

---
+++ renato/api/db.php
@@ -0,0 +1,7 @@
+<?php
+
+$mysqli = new mysqli("a2ss29.a2hosting.com", "redbeach_cmpe202user", "genemanojrenato", "redbeach_cmpe202");
+
+if ($mysqli->connect_errno) { echo "Failed to connect to MySQL: " . $mysqli->connect_error; }
+
+?>
