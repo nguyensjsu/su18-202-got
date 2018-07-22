617a29f0ecc1952d6671d7f70409ffc551166b3c
renato <my@email.com>
2018-07-21 23:31:01 -0700

    added functions to validate token

--- renato/api/functions.php
+++ renato/api/functions.php
@@ -2,7 +2,16 @@
 
 date_default_timezone_set('America/Los_Angeles');
 
+define("API_KEY", "cmpe202kungfubelikewater"); // key to protect our API from random requests
+
+
 require 'db.php';
+
+
+function getUserIdFromToken($token){
+	return $token; // for this first version, we are going to use the userId as token
+}
+
 
 
 // - - - - - - - - - - - - -
@@ -23,11 +32,40 @@ function doQueryInDatabase($query) {
 
 
 // - - - - - - - - - - - - -
+// Aux functions
+
+
+function checkAuthorization($onlyCheckAPIKey = false) {
+	if (!isset($_REQUEST["apiKey"]) || $_REQUEST["apiKey"] !== API_KEY )
+		returnErrorNotAuthenticated("Wrong API Key");
+
+	if ($onlyCheckAPIKey)
+		return true;
+
+	if (!isset($_REQUEST["token"]))
+		returnErrorNotAuthenticated("Missing User Token");
+
+	$userId = getUserIdFromToken($_REQUEST["token"]);
+	if ($userObj== false)
+		returnErrorNotAuthenticated("Invalid User Token");
+
+	return $userId;
+}
+
+
+
+
+// - - - - - - - - - - - - -
 // Return Errors
+
 
 function returnErrorDatabase(){
 	global $mysqli;
 	returnError(1, "An error occurred communicating with the database", $mysqli->error);
+}
+
+function returnErrorNotAuthenticated($errorMsgDetail){
+    returnError(2, "You are not logged in. Please sign in.", $errorMsgDetail);
 }
 
 
