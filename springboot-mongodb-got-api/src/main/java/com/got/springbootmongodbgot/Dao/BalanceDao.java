92ccb207f0f9801f582fe831ed4080b786a771b4
manojknit <erpatel@gmail.com>
2018-07-23 14:35:13 -0700

    Dao change

--- springboot-mongodb-got-api/src/main/java/com/got/springbootmongodbgot/Dao/BalanceDao.java
+++ springboot-mongodb-got-api/src/main/java/com/got/springbootmongodbgot/Dao/BalanceDao.java
@@ -1,12 +1,14 @@
 package com.got.springbootmongodbgot.Dao;
 
+import com.got.springbootmongodbgot.Entity.Balance;
 import com.got.springbootmongodbgot.Entity.Card;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+//Dependency Injection convers to dao at runtime
 @Repository
-public interface BalanceDao extends MongoRepository<Card, String> {
-    public double getTotalBalance(String UserId);
+public interface BalanceDao extends MongoRepository<Balance, String> {
+    public List<Balance> findBalanceByuserid(String UserId);
 }
--- springboot-mongodb-got-api/src/main/java/com/got/springbootmongodbgot/Dao/PaymentDao.java
+++ springboot-mongodb-got-api/src/main/java/com/got/springbootmongodbgot/Dao/PaymentDao.java
@@ -1,13 +1,11 @@
 package com.got.springbootmongodbgot.Dao;
-
-import com.got.springbootmongodbgot.Entity.Card;
+import com.got.springbootmongodbgot.Entity.Payment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-
+//Dependency Injection convers to dao at runtime
 @Repository
-public interface PaymentDao extends MongoRepository<Card, String> {
-    public double doPayment(String UserId, String Product);
+public interface PaymentDao extends MongoRepository<Payment, String> {
+    public List<Payment> findPaymentsByuserid(String UserId);
 }
